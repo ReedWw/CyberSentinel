9579c97b06cdcf0e966f68619f834e5b7005b4af
ReedWw <valip3tr@gmail.com>
2025-03-13 15:41:38 +0200

    Setting up watchdog module with basic functions

--- sentinel.py
+++ sentinel.py
@@ -1,2 +1,30 @@
 import sys
-print(sys.path)
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logging.basicConfig(stream = sys.stdout ,
+                        level = logging.INFO,
+                        format = "%(asctime)s %(message)s",
+                        datefmt = "%d.%m.%Y %H:%M:%S"
+                        )
+    logger.info("Started")
+    observer = Observer()
+    event_handler = LoggingEventHandler()
+    path = "."
+    observer.schedule(event_handler, path)
+    observer.start()
+    try:
+        while observer.is_alive():
+            observer.join(1)
+    finally:
+        observer.stop()
+        observer.join()
+        logger.info("Finished")
+
+if __name__ == '__main__':
+    main()
